9f3c127d47e744d61a68851ea3f3ef371d7702ab
Benjamin <1234hteimaj4321@gmail.com>
2020-11-07 20:23:09 -0700

    add bootstrap just in case and working on a spin animation for the projects section

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const nav = document.querySelector("nav");
 const titleWordSwap = document.querySelector("#title-word-swap");
 const references1 = document.querySelector("#references1");
 const references2 = document.querySelector("#references2");
+const proj1 = document.querySelector("#proj1");
 
 // Variables
 const titleWords = ["DESCISION", "CAREER", "FUTURE"];
 
 // Event Handlers
 function scrollHandler() {
-  console.log(pageYOffset);
+  // console.log(pageYOffset);
   if (pageYOffset > 0) {
     nav.classList.add("transparent");
   }
@@ -18,6 +19,29 @@ function scrollHandler() {
   if (pageYOffset > 700) {
     references2.classList.add("appear");
   }
+
+  // Project 1
+  if (pageYOffset > 1700 && pageYOffset < 1900) {
+    proj1.classList.add("rotate");
+    proj1.firstElementChild.classList.remove("appear");
+    proj1.lastElementChild.classList.add("appear");
+  } else {
+    proj1.classList.remove("rotate");
+    proj1.firstElementChild.classList.add("appear");
+    proj1.lastElementChild.classList.remove("appear");
+  }
+  // Project 2
+  //   if (pageYOffset > 1700 && pageYOffset < 1850) {
+  //     proj2.classList.add("rotate");
+  //     setTimeout(() => {
+  //       proj2.firstElementChild.classList.remove("appear");
+  //     }, 200);
+  //   } else {
+  //     proj2.classList.remove("rotate");
+  //     setTimeout(() => {
+  //       proj2.firstElementChild.classList.add("appear");
+  //     }, 200);
+  //   }
 }
 
 let i = 0;
